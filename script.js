6b76ecb296cbacc2bfd5830b7189febd1a05a7ab
Matija <matijac2k@gmail.com>
2023-08-25 17:34:27 +0200

    Create a way to track scores

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+console.log("********************");
+console.log("********************");
+console.log("********************");
+
+let playerPoints = 0;
+let computerPoints = 0;
+
 function getComputerChoice() {
   let randomNumber = Math.random();
 
@@ -10,7 +17,10 @@ function getComputerChoice() {
   }
 }
 
-console.log(getComputerChoice());
+function getPlayerChoice() {
+  let playerChoice;
+  return prompt("Please choose between Rock, Paper or Scissors").toLowerCase();
+}
 
 function PlayRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
@@ -20,13 +30,29 @@ function PlayRound(playerSelection, computerSelection) {
     (playerSelection === "scissors" && computerSelection === "paper") ||
     (playerSelection === "paper" && computerSelection === "rock")
   ) {
+    playerPoints += 1;
     return `You win! ${playerSelection} beats ${computerSelection}`;
   } else {
+    computerPoints += 1;
     return `You lose! ${computerSelection} beats ${playerSelection}`;
   }
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-
-console.log(PlayRound(playerSelection, computerSelection));
+function game() {
+  const result = PlayRound(getPlayerChoice(), getComputerChoice());
+  console.log(result);
+  console.log(`Player: ${playerPoints}, Computer: ${computerPoints}`);
+}
+game();
+game();
+game();
+game();
+game();
+console.log("********************");
+if (playerPoints > computerPoints) {
+  console.log("Congratulations you won against AI!");
+} else if (playerPoints < computerPoints) {
+  console.log("You lost. Terminator is coming to get you!");
+} else {
+  ("Final result is a tie!");
+}
