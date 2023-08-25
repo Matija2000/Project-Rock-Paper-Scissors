940a885f7a03d0a126c4efe01b95340165646e28
Matija <matijac2k@gmail.com>
2023-08-25 15:57:33 +0200

    Create function that plays one round of the game

--- script.js
+++ script.js
@@ -11,3 +11,22 @@ function getComputerChoice() {
 }
 
 console.log(getComputerChoice());
+
+function PlayRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return "It's a tie!";
+  } else if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "scissors" && computerSelection === "paper") ||
+    (playerSelection === "paper" && computerSelection === "rock")
+  ) {
+    return `You win! ${playerSelection} beats ${computerSelection}`;
+  } else {
+    return `You lose! ${computerSelection} beats ${playerSelection}`;
+  }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+
+console.log(PlayRound(playerSelection, computerSelection));
